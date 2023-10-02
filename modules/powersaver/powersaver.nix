aad4bf113febf4ae3101a007bc24d0af46642263
migueldeoleiros <migueldeoleiros@gmail.com>
2023-10-02 11:17:36 +0200

    Add tlp exceptions for usb devices and mouse

--- modules/powersaver/powersaver.nix
+++ modules/powersaver/powersaver.nix
@@ -25,6 +25,8 @@
       CPU_BOOST_ON_BAT = 0;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      USB_AUTOSUSPEND = 0;
+      USB_DENYLIST = "046d:c53f"; # disable suspend on mouse
     };
   };
 
